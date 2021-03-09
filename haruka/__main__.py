a7991c7dace68c4571f4c4336c5089c337a0ed49
abhijithsuhakaran2003 <77870822+abhijithsuhakaran2003@users.noreply.github.com>
2021-03-09 19:23:40 +0530

    Update_main_py
    For my identity

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -22,24 +22,17 @@ from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}!
 
-You know how hard it is sometimes to manage group so here is the solution for you
+Hey I am Your Helper😊.
 
-I'm group manager bot and Anti-spam for RR Players🃏
+Hello dear,Nowday its not easy to manage a group without a Bot.Am i corret?,its correct💯.
 
-Activate Anti-spam protection in your BM by this command : 
-/antispam on 
+Hey i give a soltion for you😊.Itz me!!! I can manage your group eaisly.⚘
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Gave all thanks to t.me/CatKing_ext my pro owner.
 
-Special thanks to [this person](t.me/denver02) for helping me.
+Add me to your chat and stay calm, I Will manage it.
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
-
-Click /help or Help button below to find out more about how to use me to my full potential.
-
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/Catqueen_bot?startgroup=true).
 """
 
 
